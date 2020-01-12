a393b38873ac2dc9be5c8a4c2d5045e5dd0b8bc9
patrick <you@example.com>
2020-01-12 10:28:15 -0600

    add select and add stuff

--- psdb.go
+++ psdb.go
@@ -2,13 +2,101 @@ package psdb
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"strings"
 )
 
-func append(filename string, record string) error {
+type DataBase struct {
+	//DataBase is the basic struct for DataBase
+	logfile string
+}
+
+func inputtomap(userinput string) (map[string]string, error) {
+	//convert the string received from CL to map for DB
+	// this should really have some error checks
+	recordmap := map[string]string{}
+	for _, kvpair := range strings.Split(userinput, " ") {
+		kvsplit := strings.SplitN(kvpair, "=", 2)
+		key := kvsplit[0]
+		value := kvsplit[1]
+		recordmap[key] = value
+	}
+	return recordmap, nil
+}
+
+func (db *DataBase) ADD(record string, ind string) error {
+	//add a record to the DB
+	//user input name=patrick home=away
+	//convert inputtstring to map
+	recordmap, _ := inputtomap(record)
+	recordjson, _ := tojson(recordmap)
+	//convert map to to JSON for write, might be cumbersome
+	dbrecord := ind + "," + recordjson
+	err := dbappend(db.logfile, dbrecord)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *DataBase) SELECT(query string) (matches []string, err error) {
+	//get a record from db
+	//user input SELECT FROM testdb * WHERE x=y
+	//convert inputstring to map
+	results := []string{}
+	querymap, _ := inputtomap(query)
+	// for each key in querymap check matching records
+	for key, val := range querymap {
+		matches, err := searchvalue(db.logfile, []string{key, val})
+		if err == nil {
+			results = append(results, matches...)
+		}
+	}
+	return matches, nil
+}
+
+func checkvalue(kvpair []string, line string) (match string, err error) {
+	//line will look like `index, {}`
+	//convert from JSON string to map
+	if len(kvpair) != 2 {
+		return "", fmt.Errorf("kvpair needs to be len 2, is %v", len(kvpair))
+	}
+	recordmap := strings.SplitN(line, ",", 2)[1]
+	linemap, _ := fromjson(recordmap)
+	if linemap[kvpair[0]] == kvpair[1] {
+		return line, nil
+	}
+	return "", fmt.Errorf("No match %v", kvpair)
+}
+
+func searchvalue(filename string, kvpair []string) (results []string, err error) {
+	// searchvalue returns for name=patrick return all records where this true
+	// checkvalue does the line by line checking
+	matches := []string{}
+	fil, err := os.Open(filename)
+	if err != nil {
+		return matches, fmt.Errorf("Cannot open file %s", filename)
+	}
+	defer fil.Close()
+
+	scan := bufio.NewScanner(fil)
+	for scan.Scan() {
+		res, err := checkvalue(kvpair, scan.Text())
+		if err == nil {
+			matches = append(matches, res)
+		}
+	}
+	return matches, fmt.Errorf("Condition %s not matched", kvpair)
+}
+
+// CREATE testdb
+// testdb ADD name=patrick age=122
+// FROM testdb SELECT name WHERE age=122
+
+func dbappend(filename string, record string) error {
 	// append appends a `record` to the file at `filename`
 	fil, openerr := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if openerr != nil {
@@ -33,7 +121,7 @@ func checkline(index string, line string) (result string, err error) {
 	return "", errors.New("Index not matched")
 }
 
-func search(filename string, index string) (result string, err error) {
+func searchindex(filename string, index string) (result string, err error) {
 	// search for `index` in `filename`
 	fil, err := os.Open(filename)
 	if err != nil {
@@ -49,6 +137,25 @@ func search(filename string, index string) (result string, err error) {
 		}
 	}
 	return "", fmt.Errorf("Index %s not matched", index)
+}
+
+func tojson(record map[string]string) (string, error) {
+	// convert map to json str
+	jsonrecord, err := json.Marshal(record)
+	if err != nil {
+		return "", err
+	}
+	return string(jsonrecord), nil
+}
+
+func fromjson(jsonobject string) (map[string]string, error) {
+	//convert json string to map
+	var unmarshaled map[string]string
+	err := json.Unmarshal([]byte(jsonobject), &unmarshaled)
+	if err != nil {
+		return map[string]string{}, err
+	}
+	return unmarshaled, nil
 }
 
 //DB needs to 1) add 2) delete 3) look up
--- psdb_test.go
+++ psdb_test.go
@@ -1,6 +1,10 @@
 package psdb
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
 
 // func Test(t *testing.T) {
 // 	got := Abs(-1)
@@ -9,16 +13,58 @@ import "testing"
 // 	}
 // }
 
-func Test_append(t *testing.T) {
-	append("dbfile.txt", "this be a test record")
-	append("dbfile.txt", "12, test record with index 12")
+func Test_checkvalue(t *testing.T) {
+
 }
 
-func Test_search(t *testing.T) {
+func Test_searchvalue(t *testing.T) {
+
+}
+
+func Test_inputtomap(t *testing.T) {
+	instring := "name=patrick age=122"
+	recmap, _ := inputtomap(instring)
+	if recmap["name"] != "patrick" {
+		t.Fatalf("got value %s for key %s", recmap["name"], "name")
+	}
+	if recmap["age"] != "122" {
+		t.Fatalf("got value %s for key %s", recmap["age"], "age")
+	}
+	fmt.Println(recmap)
+}
+
+func Test_addrecord(t *testing.T) {
+	fname := "testdb.txt"
+	testdb := new(DataBase)
+	testdb.logfile = fname
+	testdb.ADD("method=test value=record", "1")
+}
+
+func Test_append(t *testing.T) {
+	dbappend("dbfile.txt", "this be a test record")
+	dbappend("dbfile.txt", "12, test record with index 12")
+}
+
+func Test_tojson(t *testing.T) {
+	testmap := map[string]string{"key1": "value1", "key2": "value100"}
+	res, _ := tojson(testmap)
+	fmt.Printf("res to json %v", res)
+	var unmarshaled map[string]string
+	json.Unmarshal([]byte(res), &unmarshaled)
+}
+
+func Test_fromjson(t *testing.T) {
+	testmap := map[string]string{"key1": "value1", "key2": "value100"}
+	res, _ := tojson(testmap)
+	unmarshaled, _ := fromjson(res)
+	fmt.Println("\nfrom json", unmarshaled)
+}
+
+func Test_searchindex(t *testing.T) {
 	want := "good rec"
 	ind := "100"
-	append("searchfile.txt", ind+","+want)
-	res, err := search("searchfile.txt", ind)
+	dbappend("searchfile.txt", ind+","+want)
+	res, err := searchindex("searchfile.txt", ind)
 	if err != nil {
 		t.Fatalf("got error %s for index %s", err, ind)
 	}
@@ -26,7 +72,7 @@ func Test_search(t *testing.T) {
 		t.Fatalf("want %s, got %s", want, res)
 	}
 	ind = "10"
-	res, err = search("searchfile.txt", ind)
+	res, err = searchindex("searchfile.txt", ind)
 	if err == nil {
 		t.Fatalf("raise fail: did not return error for index %s", ind)
 	}
@@ -36,6 +82,6 @@ func Test_large(t *testing.T) {
 	entries := []string{"first entry", "second entry", "some text here"}
 	indices := []string{"1", "1000", "23"}
 	for num, record := range entries {
-		append("large_db.txt", indices[num]+","+record)
+		dbappend("large_db.txt", indices[num]+","+record)
 	}
 }
